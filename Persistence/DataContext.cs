c30b0428d2fbc3eb737860a3f607f2c83cfa6201
gitsneidig <scott@scottneidig.com>
2021-05-16 07:11:02 -0500

    Add files for initial skeleton

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Activity> Activities {get; set;}
+    }
+}
